6035ceb05ce82de8b820ac10f8aaf2f584088599
Justin Welenofsky <justinwelenofsky@ip-10-1-10-119.us-west-1.compute.internal>
2016-05-06 17:52:22 -0700

    WIP - Not working, euler3

--- 3/python/euler3.py
+++ 3/python/euler3.py
@@ -9,3 +9,26 @@ def is_prime(n):
 			return False
 		i += 1
 	return True
+
+def is_prime_fast(n):
+	if n % 2 == 0 or n % 3 == 0:
+		return False
+
+	sqrt_n = int(math.ceil(math.sqrt(n)))
+	i = 5
+	while(i < sqrt_n):
+		if n % i == 0 or n % (i + 2) == 0:
+			return False
+		i += 6
+
+	return True
+
+up_to = 600851475143
+largest_prime = 0
+for i in range(int(math.ceil(up_to / 2)), 1, -1):
+	if(up_to % i == 0 and is_prime_fast(i)):
+		largest_prime = i
+		print('Found')
+		break;
+
+print(largest_prime)
