dbec47879913e9fb6d312c0ff3f2518849c9ad44
Justin Welenofsky <justin@welenofsky.com>
2016-05-06 18:53:11 -0700

    eueler3

--- 3/python/euler3.py
+++ 3/python/euler3.py
@@ -1,34 +1,20 @@
 #! /usr/bin/python
 import math
-# WIP - Not done with this one
-def is_prime(n):
-	i = 2
-	half_n = int(math.floor(n / 2))
-	while i <= half_n:
-		if n % i == 0:
-			return False
-		i += 1
-	return True
 
 def is_prime_fast(n):
 	if n % 2 == 0 or n % 3 == 0:
 		return False
 
-	sqrt_n = int(math.ceil(math.sqrt(n)))
-	i = 5
-	while(i < sqrt_n):
+	for i in range(5, int(math.ceil(math.sqrt(n))), 6):
 		if n % i == 0 or n % (i + 2) == 0:
 			return False
-		i += 6
-
 	return True
 
 up_to = 600851475143
-largest_prime = 0
-for i in range(int(math.ceil(up_to / 2)), 1, -1):
-	if(up_to % i == 0 and is_prime_fast(i)):
+
+for i in range(int(math.ceil(math.sqrt(up_to))), 1, -1):
+	if(is_prime_fast(i) and up_to % i == 0):
 		largest_prime = i
-		print('Found')
 		break;
 
 print(largest_prime)
